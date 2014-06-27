3d6092ae9919f5991547030825902f813ca159e8
Chris Tierney <c.tierney@yottaa.com>
2014-06-27 13:38:04 -0400

    initial chicken

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '57cc088b6d5a45972ee229ce330d874dd9867e05e2696f1c7c7da2be4f3e70c66da65b6beb9a7dddb2a79490016bd1486f9b3d2ff5b103bfe653a2efc4801d1a'
